1f62a1348eeaae3729bc9dbfcc2c733eeaea475b
crossk <crossk@firemail.de>
2020-05-17 16:00:36 +0200

    Initial commit for a basic C# project

---
+++ WorkingShiftPlanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingShiftPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the best working shift planner of this world!");
+        }
+    }
+}
